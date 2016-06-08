f614f22ba31108bde35cb8c86237c412a00e48ac
Laxman Jangley <jangleylaxman@gmail.com>
2016-06-08 15:00:07 +0530

    tried automation of wrapping

--- src/main/scala/spark/examples/myMain.scala
+++ src/main/scala/spark/examples/myMain.scala
@@ -3,12 +3,12 @@ package spark.examples
 import org.apache.spark.{SparkConf, SparkContext}
 //import spark.examples.featurefu_wrapper
 
-object SparkGrep {
+object myMain {
 	def main(args: Array[String]) {
-//		if (args.length < 3) {
-//			System.err.println("Usage: SparkGrep <host> <input_file> <match_term>")
+		if (args.length < 3) {
+			System.err.println("Usage: SparkGrep <host> <input_file> <match_term>")
 //			System.exit(1)
-//		}
+		}
 		val ff = new df_test()
 		ff.main(args)
 //		val wraptest = new wrapTest()
